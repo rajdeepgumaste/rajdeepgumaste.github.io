93f2552089eaf11db5caba7f16dafd881c7860c7
rajdeepgumaste <rajdeepgumaste@yahoo.com>
2014-11-24 17:49:28 +0530

    adding js to render blog

--- js/data.js
+++ js/data.js
@@ -3,4 +3,11 @@ var blogApp = angular.module("blogApp", []);
 blogApp.controller("mainCtrl", ['$scope', function($scope){
 
 	$scope.test = "Hello World!";
+
+	$scope.blogs = [
+		{
+			"title" : "First Blog",
+			"content" : "This is my first blog"
+		}
+	];
 }]);
